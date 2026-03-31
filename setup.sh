dbebc32e8e1ec5641adfbe6b97af0b798783fefc
chLi <565353780@qq.com>
2026-03-31 18:37:19 +0800

    fix compile bugs

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@ pip install -U cos-python-sdk-v5
 cd ..
 mkdir MFS
 cd MFS
-bash /home/tangpei/mfs/bin/build_client.sh
+PATH="/usr/bin:$PATH" \
+  bash /home/tangpei/mfs/bin/build_client.sh
 
 TARGET_DIR=$(python -c "import site; print(site.getsitepackages()[0])")
-cp ./build_mfs_client/MFS-client/MFSClient.*.so $TARGET_DIR/
+cp ./MFSClient.*.so $TARGET_DIR/
